54b30c0babc0209dc073f93b4899fb4db0a30b4f
Valentine Ofili <valentine.ofili@gmail.com>
2020-03-22 10:02:30 +0100

    intial commit

---
+++ src/main/java/com/qucoon/dailygreeter/Services.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.qucoon.dailygreeter;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author val
+ */
+public class Services {
+    
+    Calendar mycal = Calendar.getInstance();
+    public void doGreeting(){
+        System.out.println("hello good morning, "+mycal.getTime());
+    }
+    
+    public static void main(String[] args){
+        Services myserv = new Services();
+        myserv.doGreeting();
+    }
+}
